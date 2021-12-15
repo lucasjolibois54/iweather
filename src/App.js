048831d1730d77a6010656fe761553bcb7daaa1b
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-15 22:43:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,6 +32,12 @@ const dateBuilder = (d) => {
           <div className="location">New York City, US</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
+        <div className="weather-box">
+          <div className="temp">
+            15∞
+          </div>
+          <div className="weather">sunny</div>
+        </div>
       </main>
 
     </div>
