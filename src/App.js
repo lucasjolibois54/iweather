2a922b273facda40dbfec111bdd8ed5ba5a5d63a
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-15 22:39:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ const api ={
 }
 
 function App() {
+
+const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
+}
   return (
     <div className="app">
       <main>
@@ -15,6 +27,10 @@ function App() {
               type='text'
               className='search-bar'
               placeholder='Search...'/>
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
 
