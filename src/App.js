2795eeb69ef1663f56c1c558642eb102cf5670f0
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-16 01:21:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -49,15 +49,16 @@ const dateBuilder = (d) => {
         {(typeof weather.main != "undefined") ? (
         <div className='data-container'>
           <div className='container-inside'>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
           <div className="weather-box">
             <div className="temp">
               {Math.round(weather.main.temp)}°c
             </div>
             <div className="weather">{weather.weather[0].main}</div>
+          </div>
+
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
           </div>
         </div>
