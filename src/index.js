49e5fefc88933613fe2277d242106e377604cce9
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-18 17:22:38 +0100

    reset

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './service-worker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,4 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
