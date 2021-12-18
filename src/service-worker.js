63c45bd91897f1290cc2047f6b35be03587e3bef
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-18 17:21:46 +0100

    Revert "Update service-worker.js"
    This reverts commit ad39073e61a1204190cc210b903e90fc34debcc7.

--- src/service-worker.js
+++ src/service-worker.js
@@ -32,7 +32,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/worker.js`
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
   
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
