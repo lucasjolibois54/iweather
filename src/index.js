e80eaf2fde8850734a2a8386ca9faadad2633d79
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-18 17:06:34 +0100

    serviceworker update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import * as serviceWorker from './service-worker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,4 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorkerRegistration();
+serviceWorker.register();
