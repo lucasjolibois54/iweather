01bbe15a9dcf7335a1ccc9b805478707f4b8c353
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-19 11:53:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 serviceWorkerRegistration.register();
+//npx create-react-app my-app --template cra-template-pwa
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
